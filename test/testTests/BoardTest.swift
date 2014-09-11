1b957c11fdaac8c4491371e95119e4623f5e3d7f
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2014-09-11 20:02:48 +0200

    @lplotni @jriehn @ingelheim Added Board.move

--- test/testTests/BoardTest.swift
+++ test/testTests/BoardTest.swift
@@ -5,11 +5,39 @@ class BoardTest: XCTestCase {
     
     class Board {
         let size = 25
+        var position = 0
+        
+        func move(numberOfFields: Int) {
+            position += numberOfFields
+        }
     }
     
     func testHasRightSize() {
         let board = Board()
         XCTAssertEqual(board.size, 25)
     }
+    
+    func testBoardMove() {
+        
+        let board = Board()
+        
+        XCTAssertEqual(board.position, 0)
+        board.move(2)
+        XCTAssertEqual(board.position, 2)
+        board.move(3)
+        XCTAssertEqual(board.position, 5)
+        board.move(5)
+        XCTAssertEqual(board.position, 10)
+    }
+    
+    func testBoardMoveUntilEnd() {
+        
+        let board = Board()
+        
+        XCTAssertEqual(board.position, 0)
+        board.move(26)
+        XCTAssertEqual(board.position, 25)
+    }
+
     
 }
