d465a637dc5c57327c9e71ab075886dbc6a79ea2
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2014-09-11 19:00:37 +0200

    Basic setup / empty 2D cocoa game project

---
+++ test/test/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  test
+//
+//  Created by Lukasz Plotnicki on 11/09/14.
+//  Copyright (c) 2014 Thoughtworks. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
