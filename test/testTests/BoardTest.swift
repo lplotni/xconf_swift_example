9e9e733b5f9f9e2201ae3bbc8ffe30ec87e9bc38
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2014-09-18 19:51:08 +0200

    Ready to ramboooo

--- test/testTests/BoardTest.swift
+++ test/testTests/BoardTest.swift
@@ -5,29 +5,7 @@ class BoardTest: XCTestCase {
     
     class Board {
         let size = 25
-        let snakes: [(Int,Int)] = [(24, 1)]
-        let ladders: [(Int,Int)] = [(3, 16)]
-        
-        var position = 0
-        
-        func move(numberOfFields: Int) {
-
-            var newPosition: Int? = position + numberOfFields
-            
-            let maybeResetePosition = (snakes + ladders).filter { (head, _) -> Bool in
-                return head == newPosition!
-            }.first?.1
-            
-            if let resetedPosition = maybeResetePosition {
-               newPosition = resetedPosition
-            }
-            
-             if (newPosition > size) {
-                position = size
-            } else {
-                position = newPosition!
-            }
-        }
+     
     }
     
     var board: Board!
@@ -38,49 +16,5 @@ class BoardTest: XCTestCase {
     
     func testHasRightSize() {
         XCTAssertEqual(board.size, 25)
-    }
-    
-    func testBoardMove() {
-        XCTAssertEqual(board.position, 0)
-        board.move(2)
-        XCTAssertEqual(board.position, 2)
-        board.move(3)
-        XCTAssertEqual(board.position, 5)
-    }
-    
-    func testBoardMoveUntilEnd() {
-        XCTAssertEqual(board.position, 0)
-        board.move(26)
-        XCTAssertEqual(board.position, 25)
-    }
-    
-    func testBoardMoveToSnakesHead() {
-      board.move(24)
-      XCTAssertEqual(board.position, 1)
-    }
-    
-    func testBoardMoveToLadderStart() {
-        board.move(3)
-        XCTAssertEqual(board.position, 16)
-    }
-    
-    func testAllSnakesHaveProperHeadAndTailEnd() {
-        for snake in board.snakes {
-            let (head, tailEnd) = snake
-            XCTAssertNotEqual(head, tailEnd)
-            XCTAssertLessThan(tailEnd, head)
-            XCTAssertLessThan(head, 25)
-            XCTAssertGreaterThan(tailEnd, 0)
-        }
-    }
-    
-    func testAllLaddersHaveProperStartAndEnd() {
-        for ladder in board.ladders {
-            let (start, end) = ladder
-            XCTAssertNotEqual(start, end)
-            XCTAssertLessThan(start, end)
-            XCTAssertLessThan(end, 25)
-            XCTAssertGreaterThan(start, 0)
-        }
     }
 }
--- test/testTests/DiceTest.swift
+++ test/testTests/DiceTest.swift
@@ -4,18 +4,15 @@ import XCTest
 class DiceTest: XCTestCase {
     
     class Dice {
-        class func throw() -> Int {
-            let randomNumber = Int(arc4random_uniform(6))
-            return randomNumber == 0 ? 1 : randomNumber
-        }
+        
     }
     
-    func testThrowDice() {
-        for i in 0...100 {
-            let result = Dice.throw()
-            XCTAssertGreaterThan(result, 0)
-            XCTAssertLessThan(result, 7)
-        }
-    }
+//    func testThrowDice() {
+//        for i in 0...100 {
+//            let result = Dice.throw()
+//            XCTAssertGreaterThan(result, 0)
+//            XCTAssertLessThan(result, 7)
+//        }
+//    }
     
 }
